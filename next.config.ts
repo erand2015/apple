78e02528a4e1a332eaf0b4b25b35ab2b36bb7cdf
erand2015 <erand2015@hotmail.com>
2026-03-16 19:09:59 +0100

    Final build fix

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export', // KJO ËSHTË E RËNDËSISHME
   reactCompiler: true,
-
+  images: {
+    unoptimized: true, // Duhet për export statik
+  },
   allowedDevOrigins: [
     "http://192.168.56.1",
     "http://localhost:3000",
